b8afa049ebd34de7078beff42265f49810331862
pabsaldana <pabl.saldana@profesor.duoc.cl>
2026-04-16 11:21:53 -0400

    primera carga proyecto
    se carga el proyecto inicial

---
+++ persona/src/test/java/cl/saldana/persona/PersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.saldana.persona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
