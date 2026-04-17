ba77a6de3c15403cf9b20c6ec044e1fcc8459f35
pabsaldana <pabl.saldana@profesor.duoc.cl>
2026-04-16 20:03:06 -0400

    Update PersonaRepository.java

---
+++ persona/src/main/java/cl/saldana/persona/repository/PersonaRepository.java
@@ -0,0 +1,9 @@
+package cl.saldana.persona.repository;
+
+import java.util.ArrayList;
+
+import cl.saldana.persona.modelo.Persona;
+
+public class PersonaRepository {
+    private ArrayList<Persona> personas;
+}
